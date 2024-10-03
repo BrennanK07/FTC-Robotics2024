93789ebe05e7e443b8b118b38eb5db5a966df2a5
Brennan K <70231079+BrennanK07@users.noreply.github.com>
2024-10-03 09:14:07 -0400

    Updated codebase to Android Studio

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vector2.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Vector2{
+    double x;
+    double y;
+
+    public Vector2(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2 combine(Vector2 a, Vector2 b){
+        Vector2 combined = new Vector2(0, 0);
+
+        combined.x = a.x + b.x;
+        combined.y = a.y + b.y;
+
+        return combined;
+    }
+}
