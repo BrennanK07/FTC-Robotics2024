067d46b2758db445923ad2f02e6e3a08b73338f3
Brennan K <70231079+BrennanK07@users.noreply.github.com>
2024-12-12 11:11:54 -0500

    Updated competition teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NewMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NewMain.java
@@ -9,6 +9,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "Competition TeleOp")
 
 public class Main extends LinearOpMode{
+    double oldUnixTimestamp = System.nanoTime() * 1e-9;
+    double currentUnixTimestamp = System.nanoTime() * 1e-9;
+    double deltaTime = System.nanoTime() * 1e-9;
+
+    static double ENCODER_SPEED = 5;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
@@ -16,21 +22,29 @@ public class Main extends LinearOpMode{
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
         //Linear slides
-        DcMotor bucketSlide = new DcMotor();
-        DcMotor platformSlide = new DcMotor();
+        DcMotor bucketSlide = hardwareMap.get(DcMotor.class, "motorName");
+        DcMotor platformSlide = hardwareMap.get(DcMotor.class, "motorName");
 
-        DcMotor climbSlideFront = new DcMotor();
-        DcMotor climbSlideBack = new DcMotor();
+        DcMotor climbSlideFront = hardwareMap.get(DcMotor.class, "motorName");
+        DcMotor climbSlideBack = hardwareMap.get(DcMotor.class, "motorName");
 
         //Servos
-        Servo bucketSwing = new Servo();
+        Servo bucketSwing = hardwareMap.get(Servo.class, "motorName");
 
         //CRServos
-        CRServo bucketIntake = new CRServo();
+        CRServo bucketIntake = hardwareMap.get(CRServo.class, "motorName");
+
+
+        //Initialize motors with encoders before starting
+        bucketSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bucketSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
 
         waitForStart();
 
         while (opModeIsActive()) {
+            updateDeltaTime();
+
             //Drive motors
             drive.setDrivePowers(new PoseVelocity2d(
                 new Vector2d(
@@ -41,7 +55,9 @@ public class Main extends LinearOpMode{
             ));
 
             //Linear slides
-            
+            bucketSlide.setTargetPosition(
+                bucketSlide.getCurrentPostition() + (gamepad2.left_stick_y * deltaTime * MOTOR_SPEED)
+            );
 
             //Telemetry
             drive.updatePoseEstimate();
@@ -56,5 +72,12 @@ public class Main extends LinearOpMode{
             Drawing.drawRobot(packet.fieldOverlay(), drive.pose);
             FtcDashboard.getInstance().sendTelemetryPacket(packet);
         }
+    }
+
+    private void updateDeltaTime(){
+        oldUnixTimestamp = currentUnixTimestamp;
+        currentUnixTimestamp = System.nanoTime()* 1e-9;
+
+        deltaTime = currentUnixTimestamp - oldUnixTimestamp;
     }
 }
